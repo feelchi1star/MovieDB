e9dd4e4f4838a1a2f92b5a88da054b48377ce972
feelchi1star@gmail.com <feelchi1star@gmail.com>
2023-03-03 01:25:50 +0100

    Intial Setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Provider";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
